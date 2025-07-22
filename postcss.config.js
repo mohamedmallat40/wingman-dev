1fd7e5d297e212ef3fd1f15c2bd36773fcfdabc4
mo mallat <mo_mallat>
2025-07-22 03:25:56 +0100

    feat: upgrade to tailwind v4

--- postcss.config.js
+++ postcss.config.js
@@ -1,9 +1,5 @@
-/* eslint-disable import/no-anonymous-default-export */
-
 export default {
   plugins: {
-    tailwindcss: {},
-    autoprefixer: {},
-    cssnano: {}
+    '@tailwindcss/postcss': {}
   }
 };
